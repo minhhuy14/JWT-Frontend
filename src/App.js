75c98e4afae7b74831b3abcb32dde0957caa88e8
21120177 <minhhuy14012003@gmail.com>
2024-04-27 23:14:54 +0700

    add css for login form

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav></Nav>
+        {/* <Nav></Nav> */}
         <Routes>
           <Route path="/" element={'Home'}>
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,12 +4,7 @@ const Login = (props) => {
         <div class="login-container">
             <div className="container">
                 <div className="row">
-                    <div className="content-left col-md-7 d-none">
-                        <div className="brand">
-                            Hoi Dan IT
-                        </div>
-                    </div>
-                    <div className="content-right green col-md-5">
+                    <div className="content-left col-5">
                         <div className="Detail">
                             Facebook helps you connect to all over the world.
                         </div>
@@ -18,7 +13,7 @@ const Login = (props) => {
                         <input type="text" className="form-control" placeholder="Email or phone number"></input>
                         <input type="password" className="form-control" placeholder="Password"></input>
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'><a className='forgot-password' href='#'>Forgot your password?</a></span>
                         <hr></hr>
                         <div className='text-center'>
                             <button className='btn btn-success'>Create new account</button>
